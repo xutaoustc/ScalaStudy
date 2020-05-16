fa4d81c27552323d971a728f687ca2b958bde429
xutao <xutao_ustc@163.com>
2020-05-16 09:50:38 +0800

    重新理解了， 偏函数 case class 隐式转换 隐式参数

--- src/main/scala/test/function/PartialFunctionTest.scala
+++ src/main/scala/test/function/PartialFunctionTest.scala
@@ -1,5 +1,13 @@
 package test.function
 
+// 在Scala里，我们可以使用case语句来创建一个匿名函数（函数字面量）
+// List(1,2,3) map {case i:Int=>i+1},  这很有趣，case i:Int=>i+1构建的匿名函数等同于(i:Int)=>i+1
+// case语句“额外”的好处：case语句（组合）除了可以被编译为匿名函数（类型是FunctionX，在Scala里，所有的函数字面量都是一个对象，
+// 这个对象的类型是FunctionX），还可以非常方便的编译为一个偏函数PartialFunction！（注意：PartialFunction同时是Function1的子类）
+// 编译器会根据调用处的函数类型声明自动帮我们判定如何编译这个case语句
+
+// 从collect这个示例的效果上去理解偏函数：它只对会作用于指定类型的参数或指定范围值的参数实施计算，超出它的界定范围之外的参数类型和值它会忽略
+
 object PartialFunctionTest {
   def main(args: Array[String]): Unit = {
     val list = List(1,2,3,4,"ha")
--- src/main/scala/test/implicitConversion/ImplicitFunctionTest.scala
+++ src/main/scala/test/implicitConversion/ImplicitFunctionTest.scala
@@ -2,6 +2,7 @@ package test.implicitConversion
 
 object ImplicitFunctionTest {
   def main(args:Array[String]):Unit={
+    // 指定某些数据类型之间的相互转换
     // 隐式转换函数是以implicit关键字申明的带有单个参数的函数。这种函数会自动应用,将值从一种类型转为另一种类型
     // 隐式转换函数通常在源类型或目标类型的伴生对象中，或者在当前程序作用域中，否则则需要通过import手动引入隐式转换函数
     // 当方法中的参数类型和目标类型不一致时；赋值时；当对象调用所在类中不存在的方法或成员时，编译器会自动将对象进行隐式转换
