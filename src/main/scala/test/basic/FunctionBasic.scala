ad198db98bdadfeea16db486a6e2715474dbed98
xutao <xutao_ustc@163.com>
2020-06-02 13:51:30 +0800

    another function literal usage

--- src/main/scala/test/basic/FunctionBasic.scala
+++ src/main/scala/test/basic/FunctionBasic.scala
@@ -1,5 +1,7 @@
 package test.basic
 
+import scala.collection.mutable
+
 object FunctionBasic {
   def main(args:Array[String]):Unit={
     val f1 = new Dog().wow _
@@ -7,6 +9,13 @@ object FunctionBasic {
 
     val f2 = (a1:Int)=>{println(a1)}
     f2(1)
+
+    val functionMap = new mutable.HashMap[String,(String)=>Unit]()
+    functionMap("sayHello") = sayHello
+  }
+
+  def sayHello(word:String):Unit={
+
   }
 }
 
