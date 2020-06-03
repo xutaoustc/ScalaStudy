cb56de847819aaebbf7db9849bf10a9ea5bdb60a
xutao <xutao_ustc@163.com>
2020-06-03 15:46:01 +0800

    模式匹配

---
+++ src/main/scala/test/matchCase/FunctionMatchCase.scala
@@ -0,0 +1,14 @@
+package test.matchCase
+
+import scala.collection.mutable
+
+object FunctionMatchCase {
+  def main(args:Array[String]):Unit={
+    val jobMap           = new mutable.HashMap[Long, String]
+    jobMap.foreach {
+      case (jobId, jobTimeSpan) => {
+
+      }
+    }
+  }
+}
