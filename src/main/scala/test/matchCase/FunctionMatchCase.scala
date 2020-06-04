e1c93bf657d89e807bad366c08070c937e41f2fc
xutao <xutao_ustc@163.com>
2020-06-04 21:50:30 +0800

    refine模式匹配

--- src/main/scala/test/matchCase/FunctionMatchCase.scala
+++ src/main/scala/test/matchCase/FunctionMatchCase.scala
@@ -4,11 +4,17 @@ import scala.collection.mutable
 
 object FunctionMatchCase {
   def main(args:Array[String]):Unit={
-    val jobMap           = new mutable.HashMap[Long, String]
-    jobMap.foreach {
-      case (jobId, jobTimeSpan) => {
 
+    val jobMap           = new mutable.HashMap[Long, mutable.HashMap[Long, Long]]
+    jobMap.put( 1,new mutable.HashMap[Long, Long]() )
+
+
+    jobMap.foreach {
+      case (_, value) => {
+        value.put(1,1)
       }
     }
+
+    println("ss")
   }
 }
