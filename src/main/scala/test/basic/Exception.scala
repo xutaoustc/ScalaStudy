8e271f7d42a8ffd1be7d2668e25d14e478174d8e
xutao <xutao_ustc@163.com>
2020-09-05 22:54:41 +0800

    Exception

---
+++ src/main/scala/test/basic/Exception.scala
@@ -0,0 +1,17 @@
+package test.basic
+
+import java.io.IOException
+
+object Exception {
+  def main(args: Array[String]): Unit = {
+    try {
+//      throwsException()
+    } catch {
+      case e: IllegalArgumentException => println("illegal arg. exception");
+      case e: IllegalStateException    => println("illegal state exception");
+      case e: IOException              => println("IO exception");
+    } finally {
+      println("this code is always executed");
+    }
+  }
+}
