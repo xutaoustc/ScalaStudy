162ef6e8f5648aaff957d0135fc5a3240b16a3ce
xutao <xutao_ustc@163.com>
2020-09-06 20:27:42 +0800

    OOD

---
+++ src/main/scala/test/classAndObject/OODesign.scala
@@ -0,0 +1,11 @@
+package test.classAndObject
+
+class Cat{
+  // private变量，直接生成name()和name_$eq()
+  var name:String = _  //默认值，这个时候必须指定类型
+}
+
+
+object Cat {
+
+}
---
+++ src/main/scala/test/classAndObject/TestFunctionAssignment.scala
@@ -0,0 +1,12 @@
+package test.classAndObject
+
+object TestFunctionAssignment {
+  val a, b = getVal
+
+  def getVal()={
+    println("aa")
+    1
+  }
+
+  a
+}
