ac8fbed2e8a9b60f43e8878285fb65b8103cffcc
xutao <xutao_ustc@163.com>
2020-09-07 23:12:03 +0800

    PropertyOverride

--- src/main/scala/test/e_propertyOverride/PropertyOverride.scala
+++ src/main/scala/test/e_propertyOverride/PropertyOverride.scala
@@ -1,4 +1,4 @@
-package test.propertyOverride
+package test.e_propertyOverride
 
 object PropertyOverride {
   def main(args:Array[String]):Unit={
@@ -6,7 +6,7 @@ object PropertyOverride {
     val b:B = new B
 
     // 和java中的行为完全不一样
-    // 本质是方法的复写
+    // 本质是get方法的覆盖
     println(a.name)
     println(b.name)
   }
@@ -17,5 +17,8 @@ class A{
 }
 
 class B extends A{
+  // def只能重写另外一个def
+  // val只能重写另一个val属性，或者重写不带参数的def
+  // var只能重写另一个抽象的var属性
   override val name: String = "b"
 }
