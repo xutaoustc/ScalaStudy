857e2867f854c79509b66664eaa00f3425a73f5f
xutao <xutao_ustc@163.com>
2020-10-01 23:30:23 +0800

    隐式转换

--- src/main/scala/test/g_implicitConversion/ImplicitFunctionTest.scala
+++ src/main/scala/test/g_implicitConversion/ImplicitFunctionTest.scala
@@ -7,7 +7,27 @@ object ImplicitFunctionTest {
     // 隐式转换函数通常在源类型或目标类型的伴生对象中，或者在当前程序作用域中，否则则需要通过import手动引入隐式转换函数
     // 当方法中的参数类型和目标类型不一致时；赋值时；当对象调用所在类中不存在的方法或成员时，编译器会自动将对象进行隐式转换
     implicit def double2Int(d:Double):Int=d.toInt
-
     val i:Int = 2.5
+
+    val c = new C
+    c.fun(new B)
   }
+}
+
+class C{
+  def fun(a:A):Unit={}
+}
+
+class A{}
+object A{
+  implicit def convert(a:B):A={
+    new A()
+  }
+}
+
+class B{}
+object B{
+//  implicit def convert(a:B):A={
+//    new A()
+//  }
 }
