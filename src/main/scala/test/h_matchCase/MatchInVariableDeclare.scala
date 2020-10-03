bc78b0643fc397e2c34642c33c7dc6110a53aaa4
xutao <xutao_ustc@163.com>
2020-10-03 22:02:28 +0800

    模式匹配应用于变量申明

--- src/main/scala/test/h_matchCase/MatchInVariableDeclare.scala
+++ src/main/scala/test/h_matchCase/MatchInVariableDeclare.scala
@@ -1,23 +1,21 @@
-package test.matchCase
+package test.h_matchCase
 
 object MatchInVariableDeclare {
   def main(args: Array[String]): Unit = {
+    // match中每一个case都可以单独提取出来，意思是一样的
     val (x,y) = (1,2)
 
     val Array(a,b, _*) = Array(1,2,7,9)
 
     val map = Map("a"->1)
     for((k,v)<-map){
-
     }
 
     // 只取value为1的值
     for((k,1)<-map){
-
     }
 
     for((k,v)<-map if v==0){
-
     }
   }
 }
