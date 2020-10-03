ba4a45ba601c41d1f03f2ddd7cc12a41278ad282
xutao <xutao_ustc@163.com>
2020-10-03 22:35:14 +0800

    case class

--- src/main/scala/test/h_matchCase/CaseClassMatch.scala
+++ src/main/scala/test/h_matchCase/CaseClassMatch.scala
@@ -1,7 +1,9 @@
-package test.matchCase
+package test.h_matchCase
 
 object CaseClassMatch {
   def main(args: Array[String]): Unit = {
+    //    样例类即模板类，在创建时很多方法已经提供了，使得一些重复性的工作可以减少
+
     //    在声明样例类时，下面的过程自动发生了：
     //      构造器的每个参数都成为val，除非显式被声明为var，但是并不推荐这么做；
     //      在伴生对象中提供了apply方法，所以可以不使用new关键字就可构建对象；
@@ -12,7 +14,12 @@ object CaseClassMatch {
     dollar match{
       case Dollar(v)=> println(v)
     }
+
+    val amt = Currency(10,"CNY")
+    val amt1 = amt.copy(11)
+    val amt2 = amt.copy(unit="USD")
   }
 
   case class Dollar(amount:Int)
+  case class Currency(amount:Int, unit:String)
 }
